60a65997d5dae2abd63395a5f0a6b4d86dc4d05d
barry <barry.klein@student.moringaschool.com>
2023-06-23 11:48:48 +0300

    done

--- lib/get.py
+++ lib/get.py
@@ -1,10 +1,9 @@
 import requests
 import json
 
-url = "https://learn-co-curriculum.github.io/json-site-example/endpoints/locations.json"
+url = 'https://learn-co-curriculum.github.io/json-site-example/endpoints/locations.json'
 
 response = requests.get(url)
 
-json_content = json.loads(response.text)
-
-print(json.dumps(json_content, indent=4))
+json_content=json.loads(response.text)
+print(json.dumps(json_content,sort_keys=True,indent=3 ))
